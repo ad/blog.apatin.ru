7e2948ec516fb7e1123894b29f02bbb0b0a8918a
Daniel Apatin <ad@users.noreply.github.com>
2025-06-30 12:20:37 +0300

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -26,7 +26,7 @@
         if (fired === false) {
             fired = true;
             setTimeout(() => {
-                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag_jet_beta.js", "ym");ym(21943429, "init",{clickmap:true,trackLinks:true,accurateTrackBounce:true,ecommerce:"dataLayer"});
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(21943429, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });
             }, 500);
         }
     },{passive: true});
